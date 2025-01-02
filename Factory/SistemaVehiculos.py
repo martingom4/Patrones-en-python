9c1e7300dd85da80d51d87f4d30c9f909900b330
martingom4 <martingomez9636@gmail.com>
2025-01-02 15:43:18 -0500

    Inicio de proyecto de factory con singleton

--- Factory/SistemaVehiculos.py
+++ Factory/SistemaVehiculos.py
@@ -1,23 +1,61 @@
 from abc import ABC, abstractmethod
 
 # Producto Abstracto
+""" se utiliza para definir métodos abstractos en una clase base abstracta. Un método abstracto es un método que se declara en una clase base, pero no se implementa. Las subclases que heredan de esta clase base deben proporcionar una implementación concreta de estos métodos abstractos."""
 class Carros(ABC):
     @abstractmethod
     def crear(self):
         pass
 
+    def mover(self):
+        pass
+
+    def detenerse(self):
+        pass
+
+#tambien podemos crear clases abstractas para poder heredar de ellas y poder sobreescribir los metodos pero se heredan de la prinsipal que es Carros(ABC) y se sobreescriben en las clases hijas que son los productos concretos que son crearauto, crearbus, crearbicicleta
+
+
 # Productos Concretos
 class crearauto(Carros):
+    def __init__(self):
+        self.velocidad_maxima = 200
+        self.combustible = "Gasolina"
+
     def crear(self):
         return "Auto creado"
+
+    def mover(self):
+        return "Auto en movimiento"
+
+    def detenerse(self):
+        return "Auto detenido"
+
 
 class crearbus(Carros):
     def crear(self):
         return "Bus creado"
 
+    def mover(self):
+        return "Auto en movimiento"
+
+    def detenerse(self):
+        return "Auto detenido"
+
 class crearbicicleta(Carros):
     def crear(self):
         return "Bicicleta creada"
+
+    def mover(self):
+        return "Auto en movimiento"
+
+    def detenerse(self):
+        return "Auto detenido"
+
+
+class AutoElectrico(VehiculoElectrico):
+    def crear(self):
+        return "Auto eléctrico creado"
 
 # Factory
 class CarrosFactory:
@@ -37,10 +75,7 @@ factory = CarrosFactory()
 
 auto = factory.crear_carros("auto")
 print(auto.crear())  # Auto creado
+print(auto.mover())  # El auto está en movimiento
+print(auto.detenerse())  # El auto se ha detenido
 
-bus = factory.crear_carros("bus")
-print(bus.crear())  # Bus creado
-
-bicicleta = factory.crear_carros("bicicleta")
-print(bicicleta.crear())  # Bicicleta creada
 
