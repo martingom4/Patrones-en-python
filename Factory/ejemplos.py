9c184b9c445111f62fbcf749f7338d2f21dcdaa2
martingom4 <martingomez9636@gmail.com>
2025-01-02 14:12:02 -0500

    Ejemplos de el patron de dieño factory

---
+++ Factory/ejemplos.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+
+# Producto Abstracto
+class Notificacion(ABC):
+    @abstractmethod
+    def enviar(self, mensaje):
+        pass
+
+# Productos Concretos
+class EmailNotificacion(Notificacion):
+    def enviar(self, mensaje):
+        print(f"Enviando Email: {mensaje}")
+
+class SMSNotificacion(Notificacion):
+    def enviar(self, mensaje):
+        print(f"Enviando SMS: {mensaje}")
+
+class PushNotificacion(Notificacion):
+    def enviar(self, mensaje):
+        print(f"Enviando Push: {mensaje}")
+
+# Clase Factory
+class NotificacionFactory:
+    @staticmethod
+    def crear_notificacion(tipo):
+        if tipo == "email":
+            return EmailNotificacion()
+        elif tipo == "sms":
+            return SMSNotificacion()
+        elif tipo == "push":
+            return PushNotificacion()
+        else:
+            raise ValueError("Tipo de notificación no válido")
+
+# Uso del Factory
+factory = NotificacionFactory()
+notificacion = factory.crear_notificacion("email")
+notificacion.enviar("Hola desde el patrón Factory!")
+
+notificacion_sms = factory.crear_notificacion("sms")
+notificacion_sms.enviar("Hola desde SMS!")
