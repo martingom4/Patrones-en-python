464d823372836c56730970cb4b49f19cd3dbe36e
martingom4 <martingomez9636@gmail.com>
2025-01-06 19:51:33 -0500

    Builder coche terminado

--- Builder_proyect/builder/builder_coche.py
+++ Builder_proyect/builder/builder_coche.py
@@ -1,8 +1,13 @@
 #aca pondremos las clases dedicadas a cada tipo de vehiculo
 from builder.builder_base import builder_vehiculo
 
-class builder_bus(builder_vehiculo):
+class builder_coche(builder_vehiculo):
     def agregar_motor(self):
         self.vehiculo.agregar_pieza("Motro de 500 caballos de fuerza")
+    def agregar_puertas(self):
+        self.vehiculo.agregar_pieza("3 Puertas")
+    def agregar_audio_premium(self):
+        self.vehiculo.agregar_pieza("Sistema de audio premium")
 
-        
+    
+
