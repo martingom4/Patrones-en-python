204aa25f8f2b5693012cb8ee946e0981796a0f1d
martingom4 <martingomez9636@gmail.com>
2025-01-06 19:53:43 -0500

    Builder bus terminado y funcionando

--- Builder_proyect/builder/builder_bus.py
+++ Builder_proyect/builder/builder_bus.py
@@ -1,1 +1,14 @@
 #aca pondremos las clases dedicadas a cada tipo de vehiculo
+from builder.builder_base import builder_vehiculo
+
+class builder_bus(builder_vehiculo):
+    def agregar_motor(self):
+        self.vehiculo.agregar_pieza("motor de bus 5000cc")
+
+    def agregar_puerta(self):
+        self.vehiculo.agregar_pieza("4 puertas")
+
+    def agregar_audio_premium(self):
+        self.vehiculo.agregar_pieza("audio premium")
+
+        
