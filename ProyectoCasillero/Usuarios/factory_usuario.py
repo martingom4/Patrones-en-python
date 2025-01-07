b10871e9ee00ee611d1ccf427ab242908efaf1fa
martingom4 <martingomez9636@gmail.com>
2025-01-07 10:05:47 -0500

    clase usuario y factory_usuario

---
+++ ProyectoCasillero/Usuarios/factory_usuario.py
@@ -0,0 +1,18 @@
+#aca vamos a crear dos tipos de usuarios para probar el sistema
+'''
+Propósito: Crear instancias de usuarios según su tipo.
+
+Requisitos:
+
+La Factory debe ser capaz de crear usuarios de dos tipos:
+
+Personal: Con un límite básico de paquetes (por ejemplo, 5 paquetes).
+
+Corporativo: Con un límite mayor de paquetes (por ejemplo, 50 paquetes).
+
+Valida que el tipo proporcionado sea válido. Si no lo es, lanza una excepción o devuelve un mensaje claro.
+
+
+'''
+
+
--- ProyectoCasillero/Usuarios/usuario.py
+++ ProyectoCasillero/Usuarios/usuario.py
@@ -1,1 +1,13 @@
 #clase usuario
+class usuario:
+    def __init__(self,nombre, email,tipo,limite_paquetes):
+        self.nombre = nombre
+        self.email = email
+        self.tipo = tipo
+        self.limite_paquetes = limite_paquetes
+
+
+    def __str__(self):
+        return f"Nombre: {self.nombre}, Email: {self.email}, Tipo: {self.tipo}, Limite de paquetes: {self.limite_paquetes}"
+
+    
