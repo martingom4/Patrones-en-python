417148072de791ab398b4f488ad971fa5e627a02
martingom4 <martingomez9636@gmail.com>
2025-01-07 22:07:12 -0500

    Proyecto casillero con factory y builder pattern

---
+++ ProyectoCasillero/Builder/builder_base.py
@@ -0,0 +1,47 @@
+from abc import ABC, abstractmethod
+from paquete import paquete
+'''
+Propósito: Crear diferentes configuraciones de paquetes.
+
+Requisitos:
+
+Implementa un Builder Abstracto para definir los pasos básicos:
+
+Configurar dimensiones.
+
+Configurar peso.
+
+Configurar estado inicial.
+
+Calcular costo estimado.
+
+Crea Builders Concretos para diferentes tipos de paquetes:
+
+Estándar: Dimensiones y peso típicos.
+
+Grande: Dimensiones y peso mayores.
+
+Frágil: Dimensiones normales, pero con un costo adicional por ser frágil.
+
+'''
+class builder_paquete(ABC):
+    def __init__(self):
+        self.vehiculo = paquete() # creamos un objeto de tipo paquete que ya esta definido en el archivo paquete.py
+    #aca podemos agregar los metodos que queramos para agregar los paquetes como agregar_dimensiones agregar_peso agregar_destino etc
+
+    #metodos abstracto que se va a implementar en las clases hijas
+    @abstractmethod
+    def configurar_dimensiones(self):
+        pass
+
+    @abstractmethod
+    def configurar_peso(self):
+        pass
+
+    @abstractmethod
+    def calcular_precio(self):
+        pass
+
+
+
+
---
+++ ProyectoCasillero/paquete.py
@@ -0,0 +1,15 @@
+# y esta clase es la que se encarga de crear los paquetes que se van a enviar
+class paquete:
+    def __init__(self):
+        self.paquete = [] #lista de paquetes con el metodo append se agregan los paquetes a la lista
+
+    def agregar_paquete(self, paquete):
+        self.paquete.append(paquete)
+
+    def especificaciones(self):
+        return f"El paqute contiene {','.join(self.paquete )}" #esto lo que hace es juntar todo lo que tendra el paquete y lo separa por comas
+
+
+
+
+# terminada la clase paquete se procede a crear la clase Builder para el patron Builder
